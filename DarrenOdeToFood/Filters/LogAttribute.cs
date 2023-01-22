5743666a4ef850f7f8156f4d722130c9e7832f24
Darren <Henri.jervson@gmail.com>
2023-01-22 03:05:48 +0200

    Add custom LogAttribute to CuisineController

---
+++ DarrenOdeToFood/Filters/LogAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DarrenOdeToFood.Filters
+{
+    public class LogAttribute : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
